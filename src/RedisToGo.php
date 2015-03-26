d6132d81fce41b29293eef048ff33813a5418bd5
Magnus Nordlander <magnus@nordlander.se>
2015-03-26 01:45:45 +0100

    Fixed RedisToGo

--- src/RedisToGo.php
+++ src/RedisToGo.php
@@ -3,22 +3,19 @@
 namespace Fervo\Composer\HerokuDb;
 
 use Composer\Script\Event;
-use Icecave\Lace\DatabaseDsnParser;
 
 class RedisToGo
 {
     public static function populateEnvironment(Event $event)
     {
-        $parser = new DatabaseDsnParser;
-
         $url = getenv("REDISTOGO_URL");
 
         if ($url) {
-            $options = $parser->parse($url);
+            $data = parse_url($url);
 
-            putenv("REDIS_HOST={$options['host']}");
-            putenv("REDIS_PORT={$options['port']}");
-            putenv("REDIS_AUTH={$options['user']}:{$options['password']}");
+            putenv("REDIS_HOST={$data['host']}");
+            putenv("REDIS_PORT={$data['port']}");
+            putenv("REDIS_AUTH={$data['user']}:{$data['pass']}");
         }
 
         $io = $event->getIO();
